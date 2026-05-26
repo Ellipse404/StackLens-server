5d8a7a5f2d3889e7798ded5bf7e9cfde33a2d648
Bhaskar Neogi <bhaskarneogi007@gmail.com>
2026-05-27 02:15:10 +0530

    version 0.0.2: go support

--- src/modules/documentation/documentation.service.ts
+++ src/modules/documentation/documentation.service.ts
@@ -1,26 +1,19 @@
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
+
 import { Injectable } from '@nestjs/common';
 
 import { groqClient } from '../../providers/groq/groq.provider';
 
+interface GenerateDocumentationPayload {
+  language: string;
+
+  messages: any[];
+}
+
 @Injectable()
 export class DocumentationService {
-  // async generateDocumentation(payload: any) {
-  //   const response = await groqClient.chat.completions.create({
-  //     model: 'llama-3.3-70b-versatile',
-
-  //     temperature: 0,
-  //     max_tokens: 300, // TODO: exp
-  //     messages: payload.messages,
-  //   });
-
-  //   return {
-  //     documentation: (response.choices[0]?.message?.content || '').trim(),
-  //   };
-  // }
-
-  async generateDocumentation(payload: { messages: any[] }) {
+  async generateDocumentation(payload: GenerateDocumentationPayload) {
     try {
       const response = await groqClient.chat.completions.create({
         model: 'llama-3.3-70b-versatile',
